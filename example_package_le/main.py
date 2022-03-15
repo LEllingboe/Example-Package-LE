3b5d4e35a789dc989a162dd984a678eacdef2e6c
Lachlan Ellingboe <lachlan.ellingboe@icr.ac.uk>
2022-03-15 15:33:20 +0000

    entry_points

--- example_package_le/main.py
+++ example_package_le/main.py
@@ -1,6 +1,17 @@
+import fire
+
+
 def custom_add(a: int, b: int) -> int:
     return a + b
 
 
 def hello():
     pass
+
+
+def main():
+    fire.Fire(custom_add)
+
+
+if __name__ == "__main__":
+    main()
