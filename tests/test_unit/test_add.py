31a0c4d7ff142171222d8721e00754f8c303a4d5
Lachlan Ellingboe <lachlan.ellingboe@icr.ac.uk>
2022-03-15 13:33:52 +0000

    Flake8

--- tests/test_unit/test_add.py
+++ tests/test_unit/test_add.py
@@ -1,5 +1,6 @@
-from Example_Package_LE.main import custom_add
+from example_package_le.main import custom_add
+
 
 def test_custon_add():
-    assert custom_add(1,4) == 5
-    assert custom_add(5,7) == 12
+    assert custom_add(1, 4) == 5
+    assert custom_add(5, 7) == 12
